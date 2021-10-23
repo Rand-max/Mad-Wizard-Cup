26a1089c33951fd49d0a0c9f9a5361f429689662
Rand-max <57353839+Rand-max@users.noreply.github.com>
2021-10-23 23:38:00 +0800

    first add

---
+++ Assets/Scripts/witchController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class witchController : MonoBehaviour
+{
+    public int maxSpeed;
+
+    public Animator witchani;
+
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSpeed = 1;
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVertical ();
+        SpellTrigger();
+    }
+    //idle 時上下浮動，移動速度大於0時則取消該函式
+    void MoveVertical()
+    {
+        
+        transform.position = new Vector3(transform.position.x, startPosition.y + Mathf.Sin(Time.time * maxSpeed), transform.position.z);
+
+        if(transform.position.y > 0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.y < -0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+    //發射子彈
+    void SpellTrigger()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            witchani.SetTrigger("spell");
+            //CastSpell();        
+        }
+    }
+}
